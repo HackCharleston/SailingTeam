67a6e6bec83e78a786fe483c614c8461b1a3501a
Ian Dilling <ianbjorndilling@gmail.com>
2014-04-11 19:23:01 -0400

    commit 1j

---
+++ platforms/ios/www/js/index.js
@@ -0,0 +1,22 @@
+
+var app = {
+    // Application Constructor
+    init: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+    	var deveiceReady = document.getElementById('deviceready');
+    	deveiceReady.setAttribute('style', 'display:block;');
+    }
+};
