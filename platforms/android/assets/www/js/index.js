48cec8f8d69834d9a069085cdff4c62d0bc4cd9f
Ian Dilling <ianbjorndilling@gmail.com>
2014-04-12 12:52:26 -0400

    final version for hackathon

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,22 +1,63 @@
+document.addEventListener('deviceready', onDeviceReady, false);
 
-var app = {
-    // Application Constructor
-    init: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-    	var deveiceReady = document.getElementById('deviceready');
-    	deveiceReady.setAttribute('style', 'display:block;');
-    }
-};
+function onDeviceReady() {
+	
+	$('#deviceready').css('display', 'block');
+	
+	//shake.startWatch(onShake);
+	onShake();
+}
+
+function onShake() {
+	
+	//shake.endWatch();
+	
+	$('#deviceready').fadeOut(600);
+	navigator.geolocation.getCurrentPosition(positionFound, error);
+	
+}
+
+function positionFound(position) {
+
+	var radius = 2000;
+	
+	var lat = position.coords.latitude;
+	var lon = position.coords.longitude;
+	var loc = new google.maps.LatLng(lat,lon);
+
+	map = new google.maps.Map(document.getElementById('map'), {
+	      center: loc,
+	      zoom: 15
+	    });
+	
+	service = new google.maps.places.PlacesService(map);
+	var request = {
+		location: loc,
+		radius: radius,
+		types: ['park', 'campground']
+	}
+	service.nearbySearch(request, renderResults);
+	
+}
+
+function renderResults(results, status) {
+	
+	if (status == google.maps.places.PlacesServiceStatus.OK) {
+	    
+		var container = $("#content");
+		
+		$("#content").html(results[0].photos[0].getUrl({'maxWidth': 35, 'maxHeight': 35}));
+		
+		var template = Handlebars.compile($("#searchContent").html());
+		container.append (template(results));
+		container.fadeIn(500);
+	
+	} else error();
+	
+}
+
+function error() {
+	
+	$('#content').html("No connection");
+	
+}
--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -1,22 +1,111 @@
+document.addEventListener('deviceready', onDeviceReady, false);
+var map;
+function onDeviceReady() {
+	
+	$('#deviceready').css('display', 'block');
+	
+	shake.startWatch(onShake);
+	
+}
 
-var app = {
-    // Application Constructor
-    init: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-    	var deveiceReady = document.getElementById('deviceready');
-    	deveiceReady.setAttribute('style', 'display:block;');
-    }
-};
+function onShake() {
+	
+	shake.endWatch();
+	
+	$('#deviceready').fadeOut(600);
+	$('header').animate({
+		top: '0px',
+		opacity: 1
+	}, 600);
+	$('#loading').css('display', 'block');
+	
+	navigator.geolocation.getCurrentPosition(positionFound, error);
+	
+}
+
+function positionFound(position) {
+
+	var radius = 2000;
+	
+	var lat = position.coords.latitude;
+	var lon = position.coords.longitude;
+	var loc = new google.maps.LatLng(lat,lon);
+
+	map = new google.maps.Map(document.getElementById('map'), {
+	      center: loc,
+	      zoom: 15
+	    });
+	
+	service = new google.maps.places.PlacesService(map);
+	var request = {
+		location: loc,
+		radius: radius,
+		types: ['park', 'campground']
+	}
+	service.nearbySearch(request, renderResults);
+	
+}
+
+function renderResults(results, status) {
+	
+	if (status == google.maps.places.PlacesServiceStatus.OK) {
+
+		var template = Handlebars.compile($("#searchContent").html());
+		$("#search-list").append (template(results));
+		var template = Handlebars.compile($("#page-temp").html());
+		$('#search-page').append (template(results));
+		
+		renderHome(results);
+	
+	} else error();
+	
+}
+
+function renderHome(results) {
+	
+	var container = $("#search-list");
+	
+	$('#loading').css('display', 'none');
+	container.fadeIn(500);
+	
+	$('#search-list li').on('tap', function(event) {
+		
+		container.css('display', 'none');
+		var index = $(this).data('index');
+		
+		$('header').css('display', 'none');
+		renderMap(index, results);
+		
+	});
+	
+}
+
+function renderMap(index, results) {
+	
+	$('#map, #mapcover').css('display', 'block');
+	google.maps.event.trigger(map, 'resize');
+	map.setCenter(results[index].geometry.location);
+	var marker = new google.maps.Marker({
+		position: results[index].geometry.location,
+		map: map,
+		title: results[index].name
+	});
+	
+	$('.page').eq(index).css('display', 'block');
+	
+	$('.page button').on('tap', function(event) {
+		$('.page').eq(index).css('display', 'none');
+		$('#map, #mapcover').css('display', 'none');
+		$('header').css('display', 'block');
+		marker.setMap(null);
+		renderHome(results);
+	});
+	
+	
+}
+
+function error() {
+	
+	$('#content').html("No connection");
+	
+}
